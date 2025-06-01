8471f0cba192fa47fabd72bc6c6eca8331c0ee38
MinaMagdy-mm <mina9600mm@gmail.com>
2025-06-01 22:08:32 +0300

    Upload the project

---
+++ Supermarket/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Supermarket.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StreetAdress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+
+    }
+}
